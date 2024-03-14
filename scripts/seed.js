a83aa7e63364a3922b02cbf2159483e5754fcf3b
cadenpiper <120049430+cadenpiper@users.noreply.github.com>
2024-03-14 01:45:23 -0500

    Seed DAO

---
+++ scripts/seed.js
@@ -0,0 +1,84 @@
+const hre = require("hardhat");
+const config = require('../src/config.json')
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+const ether = tokens
+
+async function main() {
+  console.log(`\nFetching accounts & network...\n`) 
+
+  const accounts = await ethers.getSigners()
+  const funder = accounts[0]
+  const investor1 = accounts[1]
+  const investor2 = accounts[2]
+  const investor3 = accounts[3]
+  const recipient = accounts[4]
+
+  let transaction, result
+
+  const { chainId } = await ethers.provider.getNetwork()
+
+  console.log(`Fetching token and transferring to accounts...\n`)
+
+  const token = await ethers.getContractAt('Token', config[chainId].token.address)
+  console.log(`Token fetched: ${token.address}\n`)
+
+  transaction = await token.transfer(investor1.address, tokens(200000))
+  result = await transaction.wait()
+
+  transaction = await token.transfer(investor2.address, tokens(200000))
+  result = await transaction.wait()
+
+  transaction = await token.transfer(investor3.address, tokens(200000))
+  result = await transaction.wait()
+
+  console.log(`Fetching dao...\n`)
+
+  const dao = await ethers.getContractAt('DAO', config[chainId].dao.address)
+  console.log(`DAO fetched: ${dao.address}\n`)
+
+  transaction = await funder.sendTransaction({ to: dao.address, value: tokens(1000) })
+  await transaction.wait()
+  console.log(`Sent funds to dao treasury...\n`)
+
+  for (var i = 0; i < 3; i++) {
+    transaction = await dao.connect(investor1).createProposal(`Propsal ${i + 1}`, ether(100), recipient.address)
+    await transaction.wait()
+
+    transaction = await dao.connect(investor1).vote(i + 1)
+    await transaction.wait()
+
+    transaction = await dao.connect(investor2).vote(i + 1)
+    await transaction.wait()
+
+    transaction = await dao.connect(investor3).vote(i + 1)
+    await transaction.wait()
+
+    transaction = await dao.connect(investor1).finalizeProposal(i + 1)
+    await transaction.wait()
+
+    console.log(`Created & Finalized Proposal ${i + 1}\n`)
+  }
+
+  console.log(`Creating one more proposal...\n`)
+
+  transaction = await dao.connect(investor1).createProposal('Proposal 4', ether(100), recipient.address)
+  await transaction.wait()
+
+  transaction = await dao.connect(investor2).vote(4)
+  await transaction.wait()
+
+  transaction = await dao.connect(investor3).vote(4)
+  await transaction.wait()
+
+  console.log(`Finished.\n`)
+
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
